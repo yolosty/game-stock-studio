e06a0a448db0dbef993d29ad1ce2eef5200456f5
queuebit <queuebit10648@gmail.com>
2016-09-13 22:04:43 -0400

    Adds react-modal on image click, and canvas with image

--- src/components/game-art-grid.js
+++ src/components/game-art-grid.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 class GameArtGrid extends Component {
+  selectArt(event) {
+    this.props.action(event.target.src);
+  }
+
   render() {
     let game = this.props.game || {};
     return (
       <div className="game-art-grid">
         {(game.images || []).map((image,i) => {
           return (
-                  <img key={image} alt={`${game.id}-achievement-${i}`} src={image+'&h=120'} />
+                  <img key={image} onClick={ this.selectArt.bind(this) } alt={`${game.id}-achievement-${i}`} src={image+'&h=120'} />
                  );
         })}
       </div>
--- src/components/game-filter.js
+++ src/components/game-filter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import GameArtGrid from './game-art-grid';
+import Modal from 'react-modal';
 
 let destiny_images = require('../destiny_images.json');
 let rocket_league_images = require('../rocket_league_images.json');
@@ -8,7 +9,13 @@ let rocket_league_images = require('../rocket_league_images.json');
 class GameFilter extends Component {
   constructor(props) {
     super(props);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.selectArt = this.selectArt.bind(this);
+    this.paintCanvas = this.paintCanvas.bind(this);
     this.state = {
+      selectedArt: undefined,
+      open: false,
       selectedGame: {},
       games: [
         { name: 'Destiny', id: 'destiny', images: destiny_images },
@@ -17,12 +24,43 @@ class GameFilter extends Component {
     };
   }
 
+  openModal () { this.setState({open: true}); }
+
+  closeModal () { this.setState({open: false}); }
+
   handleChange(event) {
     this.setState({
       selectedGame: this.state.games.find((x) => {
         return x.id === event.target.value;
       } )
     });
+  }
+
+  paintCanvas() {
+    let context = document.getElementById('art-canvas').getContext("2d");
+    let artImg = new Image();
+
+    artImg.src = this.state.selectedArt + '&h=720';
+    context.canvas.width = 1280;
+    context.canvas.height = 720;
+    artImg.onload = function () {
+        context.drawImage(artImg, 0, 0, 1280, 720);
+    }
+  }
+
+  downloadStock(event) {
+    let targetDevice = event.target.id;
+
+    if (targetDevice === 'iphone-dl') {
+    } else if (targetDevice === 'android-dl') {
+    }
+  }
+
+  selectArt(img) {
+    this.setState({
+      selectedArt: img.substring(0,img.length - 6)
+    });
+    this.openModal();
   }
 
   render() {
@@ -39,7 +77,16 @@ class GameFilter extends Component {
                    );
           })}
         </select>
-        <GameArtGrid game={this.state.selectedGame}/>
+        <GameArtGrid game={this.state.selectedGame} action={this.selectArt} />
+        <Modal isOpen={this.state.open} onAfterOpen={this.paintCanvas}>
+          <h1>Achievement art</h1>
+          <canvas id='art-canvas' />
+          <div>
+            <button id='iphone-dl' onClick={this.downloadStock.bind(this)}>iPhone</button>
+            <button id='android-dl' onClick={this.downloadStock.bind(this)}>Android</button>
+            <button onClick={this.closeModal}>Close</button>
+          </div>
+        </Modal>
       </div>
     );
   }
